6a8669493e57af162e64cadb6d9a4add543011c8
Jeff Boulter <jboulter@hubspot.com>
2015-11-22 13:58:56 -0500

    added calendar job

---
+++ kitchen/jobs/calendar.rb
@@ -0,0 +1,34 @@
+require 'icalendar'
+require 'net/http'
+require 'net/https'
+require_relative 'config'
+
+uri = URI ICAL_URL
+
+puts ICAL_URL
+
+SCHEDULER.every '15m', :first_in => 0 do |job|
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  response = http.request(Net::HTTP::Get.new(uri.path))
+  result = response.body
+  calendars = Icalendar.parse(result)
+  calendar = calendars.first
+
+  events = calendar.events.map do |event|
+    {
+      start: event.dtstart,
+      end: event.dtend,
+      summary: event.summary,
+      is_all_day: event.dtstart.class.name == "Icalendar::Values::Date" 
+    }
+  end.select { |event| event[:start] > DateTime.now }
+
+  events = events.sort { |a, b| a[:start] <=> b[:start] }
+  
+  puts "loaded #{ events.length } upcoming calendar events"
+  events = events[0..8]
+  
+  send_event('calendar', { events: events })
+end
