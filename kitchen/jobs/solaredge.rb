e6fa6b00cb9c05613aa0ecdaa4dc20fc67763e26
Jeff Boulter <jboulter@hubspot.com>
2016-01-08 22:00:25 -0500

    add solaredge job

---
+++ kitchen/jobs/solaredge.rb
@@ -0,0 +1,18 @@
+require 'net/https'
+require 'json'
+require_relative 'config'
+
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  http = Net::HTTP.new("monitoringapi.solaredge.com", 443)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  response = http.request(Net::HTTP::Get.new("/site/#{SOLAREDGE_SITE_ID}/overview.json?api_key=#{SOLAREDGE_API_KEY}"))
+  data = JSON.parse(response.body)
+  current_power = data["overview"]["currentPower"]["power"]
+  today_energy = data["overview"]["lastDayData"]["energy"]
+
+  puts "energy: #{today_energy}"
+  puts "current: #{current_power}"
+
+  send_event('solaredge', { current_power: "#{current_power}", today_energy: "#{today_energy}" })
+end
